285bd7b4492b5b80eee4fd39bb318204e9d4e957
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-04-13 12:39:54 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SecondSemestr_queue
+# SecondSemestr_tree-0.0.1
+
+[![Build Status](https://travis-ci.org/SashaPozhuev1/SecondSemestr_tree-0.0.1.svg?branch=master)](https://travis-ci.org/SashaPozhuev1/SecondSemestr_tree-0.0.1)
+
+```
+cmake -H. -B_builds -DBUILD_TESTS=ON
+cmake --build _builds
+cmake --build _builds --target test -- ARGS=--verbose
+```
---
+++ tests/tree.cpp
@@ -0,0 +1,116 @@
+#include <catch.hpp>
+#include <sstream>
+
+#include "tree.hpp"
+
+TEST_CASE("creating tree")
+{
+	tree_t<int> tree;
+	REQUIRE( tree.root() == nullptr );
+}
+
+TEST_CASE("insert and print tree")
+{
+	std::string input{
+		"        ----9\n"
+		"                ----8\n"
+		"            ----7\n"
+		"    ----7\n"
+		"        ----6\n"
+		"----5\n"
+		"        ----4\n"
+		"    ----4\n"
+		"        ----3\n"};
+	
+	tree_t<int> tree { 5, 7, 4, 6, 9, 7, 8, 3, 4 };
+	
+	std::ostringstream ostream;
+	tree.print(ostream);
+	REQUIRE( input == ostream.str() );
+}
+
+TEST_CASE("find tree")
+{
+	tree_t<int> tree;
+	
+	tree.insert(5);
+	tree.insert(7);
+	tree.insert(4);
+	
+	REQUIRE( tree.find(5) == true );
+	REQUIRE( tree.find(7) == true );
+	REQUIRE( tree.find(0) == false );
+}
+
+TEST_CASE("remove tree")
+{
+	tree_t<int> tree { 5, 7, 4 };
+	
+	REQUIRE( tree.remove(6) == false );
+	REQUIRE( tree.remove(7) == true );
+	REQUIRE( tree.remove(5) == true );
+	REQUIRE( tree.remove(4) == true );
+}
+
+TEST_CASE("compare tree")
+{
+	tree_t<int> A { 5, 7, 4 };
+	tree_t<int> B { 5, 7, 4 };
+	tree_t<int> C { 5, 7, 4, 4 };
+	
+	REQUIRE( (A == B) == true );
+	REQUIRE( (C == B) == false );
+}
+
+TEST_CASE("read tree")
+{
+	tree_t<double> tree;
+	
+	bool success = true;
+	for(std::size_t i = 0; i < 5; ++i) {
+		if (i == 0) {
+			std::string input{"+9.8"};
+			
+			std::istringstream istream(input);
+			std::ostringstream ostream;
+		
+			success = tree.read(istream, ostream);
+			REQUIRE( success == true );
+		}
+		else if (i == 1) {
+			std::string input{"="};
+			std::string output{"----9.8\n"};
+			std::istringstream istream(input);
+			std::ostringstream ostream;
+		
+			success = tree.read(istream, ostream);
+			REQUIRE( success == true );
+			REQUIRE( output == ostream.str() );
+		}
+		else if (i == 2) {
+			std::string input{"?1"};
+			std::string output{"false"};
+			std::istringstream istream(input);
+			std::ostringstream ostream;
+			
+			success = tree.read(istream, ostream);
+			REQUIRE( success == true );
+		}
+		else if (i == 3) {
+			std::string input{"d"};
+			std::istringstream istream(input);
+			std::ostringstream ostream;
+		
+			success = tree.read(istream, ostream);
+			REQUIRE( success == true );
+		}
+		else if (i == 4) {
+			std::string input{"q"};
+			std::istringstream istream(input);
+			std::ostringstream ostream;
+		
+			success = tree.read(istream, ostream);
+			REQUIRE( success == false );
+		}
+	}
+}
